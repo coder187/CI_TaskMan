818e72de4643401b54d0a4a7893fffd9823f7950
coder187 <coder187@hotmail.com>
2021-08-07 09:42:17 +0000

    add login page

--- app.py
+++ app.py
@@ -30,6 +30,27 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
+@app.route("/login.html", methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"],request.form.get("password")):
+                    flash("Welcome, {}".format(request.form.get("username")))
+                    session["user"] = request.form.get("username").lower()
+
+            else:
+                flash("Username and/or Password Incorrect")
+        
+        else:
+            flash("Username and/or Password Incorrect")
+
+    return render_template("login.html")
+
+
 @app.route("/register.html", methods=['POST', 'GET'])
 def register():
     if request.method == "POST":
